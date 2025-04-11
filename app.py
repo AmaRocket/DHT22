dd19f66461e0c240d2d9626a086824f382f3135e
AmaRocket <alexeverdie999@gmail.com>
2025-04-11 11:27:53 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,87 @@
+import json
+import flask_socketio as flask_socketio
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+from random import random
+from threading import Lock
+from datetime import datetime
+
+
+#from dht22_module import DHT22Module
+#import board
+#dht22_module = DHT22Module(board.D18)
+
+import platform
+
+if platform.system() == "Linux":
+    from dht22_module import DHT22Module
+    import board
+    dht22_module = DHT22Module(board.D18)
+else:
+    # Mock for local dev on Mac
+    class MockDHT22Module:
+        def get_sensor_readings(self):
+            # Return some fake readings for dev/test
+            return 66.5, 66.5
+
+    dht22_module = MockDHT22Module()
+
+
+thread = None
+thread_lock = Lock()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+"""
+Background Thread
+"""
+
+
+def background_thread():
+    while True:
+        temperature, humidity = dht22_module.get_sensor_readings()
+        sensor_readings = {
+            "temperature": temperature,
+            "humidity": humidity,
+        }
+        sensor_json = json.dumps(sensor_readings)
+
+        socketio.emit("updateSensorData", sensor_json)
+        socketio.sleep(3)
+
+
+"""
+Serve root index file
+"""
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+"""
+Decorator for connect
+"""
+
+
+@socketio.on("connect")
+def connect():
+    global thread
+    print("Client connected")
+
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+
+
+"""
+Decorator for disconnect
+"""
+
+
+@socketio.on("disconnect")
+def disconnect():
+    print("Client disconnected", request.sid)
---
+++ dht22_module.py
@@ -0,0 +1,31 @@
+# import Adafruit_DHT
+try:
+    import Adafruit_DHT
+except ImportError:
+    import mock_Adafruit_DHT as Adafruit_DHT
+
+import time
+
+
+class DHT22Module:
+    def __init__(self, pin=4):
+        self.dht_device = Adafruit_DHT.DHT22
+
+    def get_sensor_readings(self):
+        while True:
+            try:
+                sensor = Adafruit_DHT.DHT22
+                pin = 4
+                humidity, temperature_c = Adafruit_DHT.read_retry(sensor, pin)
+                print(f"Temp: {temperature_c} C    Humidity: {humidity}% " )
+                return temperature_c, humidity
+
+            except RuntimeError as error:
+                # Errors happen fairly often, DHT's are hard to read, just keep going
+                print(error.args[0])
+                time.sleep(2.0)
+                continue
+            except Exception as error:
+                print(error)
+                raise error
+
