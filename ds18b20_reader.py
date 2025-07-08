d43d02c91031b83de8694a4b66ad870bb4321d79
AmaRocket <alexeverdie999@gmail.com>
2025-07-08 15:22:29 +0200

    add test ds18b20 sensor reader_1

--- ds18b20_reader.py
+++ ds18b20_reader.py
@@ -1,32 +1,59 @@
 import os
 import glob
 import time
-import sys
+import datetime
 
-base_dir = '/sys/bus/w1/devices/'
-device_folders = glob.glob(base_dir + '28*')
+# these tow lines mount the device:
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
-if not device_folders:
-    print("❌ No DS18B20 sensor detected! Check wiring and reboot.")
-    sys.exit(1)
 
-device_file = device_folders[0] + '/w1_slave'
+class DS18B20:
+    def __init__(self):
+        self.base_dir = r'/sys/bus/w1/devices/28*'
+        self.sensor_path = []
+        self.sensor_name = []
+        self.temps = []
+        self.log = []
 
-def read_temp_raw():
-    with open(device_file, 'r') as f:
-        return f.readlines()
+    def find_sensors(self):
+        self.sensor_path = glob.glob(self.base_dir)
+        self.sensor_name = [path.split('/')[-1] for path in self.sensor_path]
 
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos + 2:]
-        temp_c = float(temp_string) / 1000.0
-        return temp_c
+    def strip_string(self, temp_str):
+        i = temp_str.index('t=')
+        if i != -1:
+            t = temp_str[i + 2:]
+            temp_c = float(t) / 1000.0
+            temp_f = temp_c * (9.0 / 5.0) + 32.0
+        return temp_c, temp_f
+
+    def read_temp(self):
+        tstamp = datetime.datetime.now()
+        for sensor, path in zip(self.sensor_name, self.sensor_path):
+            # open sensor file and read data
+            with open(path + '/w1_slave', 'r') as f:
+                valid, temp = f.readlines()
+            # check validity of data
+            if 'YES' in valid:
+                self.log.append((tstamp, sensor) + self.strip_string(temp))
+                time.sleep(2)
+            else:
+                time.sleep(0.2)
+
+    def print_temps(self):
+        print('-' * 90)
+        for t, n, c, f in self.log:
+            print(f'Sensor: {n}  C={c:,.3f}  F={f:,.3f}  DateTime: {t}')
+
+    def clear_log(self):
+        s.log.clear()
+
+
+s = DS18B20()
+s.find_sensors()
 
 while True:
-    print(f"Temperature: {read_temp():.2f}°C")
-    time.sleep(2)
+    s.read_temp()
+    s.print_temps()
+    s.clear_log()
